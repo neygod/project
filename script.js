0e6b7dd72068262b05281fc50e5f31bc21be571f
Dmitriy <o01i@yandex.ru>
2022-08-01 13:51:42 +0300

    задача 5 упражнения 23

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
-// Место для первой задачи
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте 
+//строку в ней
 
-    // Пишем решение вот тут
-       for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
-        
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j <lines-i; j++) {
+        result=result+' ';
+
     }
-    
-    // Не трогаем
-    return result;
-}
+    for (let k = 0; k <=i*2; k++) {
+        result=result+'*';
 
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    // Пишем решение вот тут
-    for (let i = 0; i < data.length; i++) {
-        if (typeof(data[i])==='number') {
-            data[i]=data[i]*2;continue;
-        }
-        data[i]=data[i]+" - done";
-        
     }
-    
-    
-    // Не трогаем
-    return data;
+        result=result+'\n';
 }
-
-// Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-    // Пишем решение вот тут
-      for (let i = data.length-1; i >= 0; i--) {
-        result[data.length-1-i] = data[i];
-    } 
-    // Не трогаем
-    return result;
-}
+console.log(result);
