0f3986820461d791b34e890402bc8b5e07b791d8
Dmitriy <o01i@yandex.ru>
2022-08-04 12:49:44 +0300

    Urok 32

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-function fib(index) {
-    let finStroka = '0', fibB=1, fibA=0, xx;
-    if (index===0 || typeof(index) !== "number"|| !Number.isInteger(index)) {
-         return finStroka = '';
-        }
-    if (index===1) {
-         return finStroka;
-        }
+// function first() {
+   
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+//     function second () {
+//         console.log(2);
 
-        for (let i = 2; i <=index; i++) {
-           
-            finStroka =  finStroka+` ${fibB}`;
-            xx=fibB;
-            fibB=fibB+fibA;fibA=xx;
-        }
-          
-
-    return finStroka;
+//     }
     
-}
-fib(5)
+// }
+// first();
+// second();
+
+
+// function LearnJS(lang, callback) {
+//   console.log (`я учу: ${lang}`);
+//   callback();
+//   }
+//   function done() {
+//     console.log ('я прошел этот урок');
+//   }
+//   LearnJS('JS', done);
+
+
+//const obj = new Object();
+'use strict'
+const options = {
+    name: 'test',
+    width: 1024,
+    hight: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log ('test');
+    }
+    
+};
+options.makeTest();
+const {border, bg} = options.colors;
+
+console.log(Object.keys(options).length);
+// let counter =0;
+// for(let key in options) {
+//     if (typeof (options[key])==='object') {
+//         for (let i in options[key] ) {
+//             console.log(`Свойсвто ${i} имееет знание ${options[key][i]}`);
+//             counter++;
+//     } 
+// } else {
+    
+//     console.log(`Свойсвто ${key} имееет знание ${options[key]}`);
+//     counter++;
+// }
+// }
+// console.log(counter)
