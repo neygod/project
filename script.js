a101af5857d7b4c68f8c24b08c0c04644271f834
Dmitriy <o01i@yandex.ru>
2022-08-06 10:19:01 +0300

    задача 35\11

--- script.js
+++ script.js
@@ -1,97 +1,32 @@
-
-// function showExperience(plan) {
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+// console.log(family.length);
+// function showFamily(arr) {
+//      let familyText =''; 
+//      let familyBul = false;
+//     arr.forEach(element => {familyText = familyText+' '+element;
+//      familyBul=true;
      
-//     for( let key in plan) {
-//         if (key==='exp') { 
-//              return plan[key];
-//         }
-//         if (typeof(plan[key])==="object") {
-//             for( let i in plan[key]) {
-//                 if (i==='exp') { 
-//                       return plan[key][i];
-//                 }
-
-//         }
+//     });
+//     if (familyBul===false) {return'Семья пуста'}
+//     else {
+//     return 'Семья состоит из:'+familyText;    
 //     }
-// }
-// }
-
-// function showProgrammingLangs(plan) {
-//     let langs= '', answer= '';
     
-//     for( let key in plan) {
-//      if (key==="programmingLangs") {
-//           langs = plan[key];
-//      }
-//      if (typeof(plan[key])==='object')  {
-//          for (let key2 in plan[key]) {
-//              if (key2==="programmingLangs") {
-//                  langs =plan[key][key2];
-//              }
-//              if (typeof(plan[key][key2])==='object') {
-//                  for (let key3 in plan[key][key2]) {
-//                      if (key3==="programmingLangs") {
-//                          langs =plan[key][key2][key3];
-//                      }
-//                  }
-//              }
-
-//          }
-         
- 
-//  }
- 
 // }
+// showFamily(family);
 
-// for( let key4 in langs) {
-//  answer=answer+`Язык ${key4} изучен на ${langs[key4]}`;
-//  answer=answer+'\n';
-// }
-// return answer; 
-// }
-//  showExperience(personalPlanPeter)
-//  showProgrammingLangs(personalPlanPeter);
 
- 'use strict'
- const personalPlanPeter = {
-     name: "Peter",
-     age: "29",
-     skills: {
-         languages: ['ru', 'eng'],
-         programmingLangs: {
-             js: '20%',
-             php: '10%'
-         },
-         exp: '1 month'
-     },
-     showAgeAndLangs: function (plan) {
-        let answer='';
-        for( let key in plan) {
-            if (key==="age") {
-             answer = answer+`Мне ${plan[key]} и я владею языками:`;
-                         }
-        }
-        for( let key1 in plan) {
-            if ( typeof(plan[key1])==="object") {
-                for( let key2 in plan[key1]) {
-                     if (key2==="languages") {
-                        for (let key3 in plan[key1][key2]) {
-                  answer = answer+`${plan[key1][key2][key3]} `.toUpperCase();
-                        }
-               
-                
-                
-             
-            }
-        }
-      
-        
-        
-    }
- 
+
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+
+function standardizeStrings(arr) {
+    let cityText='';
+     arr.forEach(element => {cityText = cityText+element+'\n';
+                });
+        console.log(cityText=cityText.toLocaleLowerCase(), typeof(cityText));
+     
  }
- console.log(answer);
-}
- }
- personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
+    standardizeStrings(favoriteCities); 
