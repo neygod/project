38f84f697ac5280efc5c6fae929c231679e5e01d
Dmitriy <o01i@yandex.ru>
2022-08-07 10:25:50 +0300

    задача 36\14

--- script.js
+++ script.js
@@ -1,52 +1,142 @@
 'use strict';
 
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+// Эти группы должны быть массивами. Как итог, функция
+//  возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+function sortStudentsByGroups(arr) {
+    let arrTeam = [];
+    let x = [];
+    arr.sort();
+       for (let i=0; i<Math.floor(arr.length/3);i++) {
+        for (let j=0; j<3; j++) {
+            x[j] = arr[i*3+j]
         }
-        
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-}
+        arrTeam[i] = [x[0],x[1],x[2]]
+          
+    }
+    if (arr.length%3 === 0) {
+        arrTeam.push('Оставшиеся студенты: -')}
+        if (arr.length%3 ===1) {
+            arrTeam.push(`Оставшиеся студенты: ${arr[arr.length-1]}`)
+        }
+        if (arr.length%3 ===2) {
+            arrTeam.push(`Оставшиеся студенты: ${arr[arr.length-2]}, ${arr[arr.length-1]}`)   
+        }
 
-function isBudgetEnough(data) {
-    let shopSQs =0;
+    return arrTeam;
+
+}
+sortStudentsByGroups(students);
+
+
+
+
+
+
+
+
+
+// function sortStudentsByGroups(arr) {
+//     let arrTeam = [];
+//     arr.sort();
+//     for (let i=0, j=0; i<Math.floor(arr.length/3);i++) {
+//         arrTeam [i] = [arr[i+j], arr[i+1+j], arr[i+2+j]];j=j+3;
+//             }
+   
     
-    data.shops.forEach((i, a) => {
-        let shopSQ=1
-        for ( let key in i) {
-            shopSQ=shopSQ*i[key];
+//     let team = Math.floor( arr.length/3);
+//     console.log(arrTeam);
+   
 
-        };
-        shopSQs=shopSQs+shopSQ;
+// }
+// sortStudentsByGroups(students);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
         
-    })
-    if (data.budget >= data.height*shopSQs*data.moneyPer1m3) {
-         return 'Бюджета достаточно';
-            } else {
-            return 'Бюджета недостаточно';   
-            };
-   
-   
-}
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
 
-isBudgetEnough(shoppingMallData);
+// function isBudgetEnough(data) {
+//     let shopSQs =0;
+    
+//     data.shops.forEach((i, a) => {
+//         let shopSQ=1
+//         for ( let key in i) {
+//             shopSQ=shopSQ*i[key];
+
+//         };
+//         shopSQs=shopSQs+shopSQ;
+        
+//     })
+//     if (data.budget >= data.height*shopSQs*data.moneyPer1m3) {
+//          return 'Бюджета достаточно';
+//             } else {
+//             return 'Бюджета недостаточно';   
+//             };
+   
+   
+// }
+
+// isBudgetEnough(shoppingMallData);
 
 
 
