717caf7f2fab327e5efc382eae314b0c6f611c2f
Dmitriy <o01i@yandex.ru>
2022-08-08 11:15:05 +0300

    задача 41

--- script.js
+++ script.js
@@ -1,248 +1,60 @@
 'use strict';
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-// Эти группы должны быть массивами. Как итог, функция
-//  возвращает новый массив с тремя командами и строкой как 4й элемент.
-
-function sortStudentsByGroups(arr) {
-    let arrTeam = [];
-    let x = [];
-    arr.sort();
-       for (let i=0; i<Math.floor(arr.length/3);i++) {
-        for (let j=0; j<3; j++) {
-            x[j] = arr[i*3+j]
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
         }
-        arrTeam[i] = [x[0],x[1],x[2]]
-          
-    }
-    if (arr.length%3 === 0) {
-        arrTeam.push('Оставшиеся студенты: -')}
-        if (arr.length%3 ===1) {
-            arrTeam.push(`Оставшиеся студенты: ${arr[arr.length-1]}`)
-        }
-        if (arr.length%3 ===2) {
-            arrTeam.push(`Оставшиеся студенты: ${arr[arr.length-2]}, ${arr[arr.length-1]}`)   
-        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-    return arrTeam;
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
 
+    return answer;
 }
-sortStudentsByGroups(students);
 
+console.log(isOpen(restorantData.openNow))
 
+function isAverageLunchPriceTrue(fDish, sDish, average) {
 
-
-
-
-
-
-
-// function sortStudentsByGroups(arr) {
-//     let arrTeam = [];
-//     arr.sort();
-//     for (let i=0, j=0; i<Math.floor(arr.length/3);i++) {
-//         arrTeam [i] = [arr[i+j], arr[i+1+j], arr[i+2+j]];j=j+3;
-//             }
    
     
-//     let team = Math.floor( arr.length/3);
-//     console.log(arrTeam);
-   
-
-// }
-// sortStudentsByGroups(students);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const shoppingMallData = {
-//     shops: [
-//         {
-//             width: 10,
-//             length: 5
-//         },
-//         {
-//             width: 15,
-//             length: 7
-//         },
-//         {
-//             width: 20,
-//             length: 5
-//         },
-//         {
-//             width: 8,
-//             length: 10
-//         }
-        
-//     ],
-//     height: 5,
-//     moneyPer1m3: 30,
-//     budget: 50000
-// }
-
-// function isBudgetEnough(data) {
-//     let shopSQs =0;
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
     
-//     data.shops.forEach((i, a) => {
-//         let shopSQ=1
-//         for ( let key in i) {
-//             shopSQ=shopSQ*i[key];
+}
 
-//         };
-//         shopSQs=shopSQs+shopSQ;
-        
-//     })
-//     if (data.budget >= data.height*shopSQs*data.moneyPer1m3) {
-//          return 'Бюджета достаточно';
-//             } else {
-//             return 'Бюджета недостаточно';   
-//             };
-   
-   
-// }
-
-// isBudgetEnough(shoppingMallData);
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
 
 
+function transferWaitors(data) {
+    const copy = JSON.parse(JSON.stringify(data));
 
+    copy.waitors[0] = {name: 'Mike', age: 32};
+    return copy;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const personalMovieDB = {
-//     count: 0,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-//     start: function () {
-//         personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    
-//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-//             personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//         }
-//     },
-//     rememberMyFilms: function () {
-//         for (let i = 0; i < 2; i++) {
-//             const a = prompt('Один из последних просмотренных фильмов?', ''),
-//                   b = prompt('На сколько оцените его?', '');
-        
-//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//                 personalMovieDB.movies[a] = b;
-//                 console.log('done');
-//             } else {
-//                 console.log('error');
-//                 i--;
-//             }
-//         }
-//     },
-//     detectPersonalLevel: function () {
-//         if (personalMovieDB.count < 10) {
-//             console.log("Просмотрено довольно мало фильмов");
-//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//             console.log("Вы классический зритель");
-//         } else if (personalMovieDB.count >= 30) {
-//             console.log("Вы киноман");
-//         } else {
-//             console.log("Произошла ошибка");
-//         }
-//     },
-//      showMyDB: function (hidden) {
-//         if (!hidden) {
-//             console.log(personalMovieDB);
-//         }
-//     },
-//     toggleVisibleMyDB: function () {
-//         if (personalMovieDB.privat) {
-//             personalMovieDB.privat=false;
-
-//         } else {
-//             personalMovieDB.privat=true;
-
-//         }
-
-//     },
-//      writeYourGenres: function() {
-//         for (let i = 1; i < 2; i++) {
-//             let genres =prompt(`Ваш любимый жанры через запятую`).toLocaleLowerCase();
-// if (genres ==='' || genres == null) {
-//     console.log('Вы ввели некоррктные данные или не ввели их вовсе');
-//     i--;
-// } else {
-//     personalMovieDB.genres = genres.split(',');
-//     personalMovieDB.genres.sort();
-
-// }
-
-
-           
-//         }
-//         personalMovieDB.genres.forEach(( item, i) => {
-//             console.log(`Любимый жанр ${i+1} - это ${item}`);
-//         });
-//     }
-// };
-
-
-
-
-
-
-
-
-
+transferWaitors(restorantData);
