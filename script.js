cd7b32219e89349e6bf87a305df346d2911b1b2d
Dmitriy <o01i@yandex.ru>
2022-08-22 17:13:23 +0300

    exempler 61/17

--- script.js
+++ script.js
@@ -1,88 +1,52 @@
 'use strict';
-
-
-
-function factorial(x) {
-    if(typeof(x)==='number'&& Number.isInteger(x) && x>0) {
+function amountOfPages(summary) {
+ let summArr = [],
+     k = 0;
+for (let i = 1; i <= summary; i++) {
+    for (let j = 0; j <String(i).length; j++) {
+        summArr.push(Number(String(i)[j]));
+        k=j+1;
         
-    let result = 1;
-    if (x===1) {
-        return result*x;      
-    } else {
-return x*factorial(x-1);
     }
-} else if (x<=0) {
-    return 1;
-    } else {
-        return 'ошибка';
+    if (summArr.length==summary) {
+        break;
     }
-
+  
 }
-factorial(10);
+
+return Number((summArr.slice(summArr.length-k)).join(''));
+}
+amountOfPages(1095);
 
 
 
 
 
-// function pow (x,n) {
-//     let result = 1;
-//     for (let i=0; i<n; i++) {
-//         result*=x;
 
-//     }
-//     return result;
+
+
+
+
+
+
+
+
+
+// if (String(i).length==1) {
+        
 // }
-
-// function pow (x,n) {
-//     if (n===1) {
-//         return x;
-//     } else {
-//         return x*pow(x, n-1);
-//     }
-//     }
-//     return result;
+// if (String(i).length==2) {
+//     summArr.push(Number(String(i)[0]));
+//     summArr.push(Number(String(i)[1]));
 // }
-
-
-// function getTotalProgressIteration(data) {
-//     let total = 0;
-//     let student = 0;
-// for (let  course of Object.values) {
-//     if (Array.isArray(course)) {
-// student += course.length;
-// for (i=0;i<course.length; i++) {
-//     total=+course[i].progress;
+// if (String(i).length==3) {
+//     summArr.push(Number(String(i)[0]));
+//     summArr.push(Number(String(i)[1]));
+//     summArr.push(Number(String(i)[2]));
 // }
-//     } else {
-//         for (let subcourse of Object.values(course)) {
-//             student += subcourse.length;
-//             total=+subcourse[i].progress;
+// if (String(i).length==4) {
+//     summArr.push(Number(String(i)[0]));
+//     summArr.push(Number(String(i)[1]));
+//     summArr.push(Number(String(i)[2]));
+//     summArr.push(Number(String(i)[3]));
 // }
-//     }
- 
-// }
-//     return total/student;
-// }
-
-// function getTotalProgressByRecursion(data) {
-//     if (Array.isArray(data)) {
-//        let total = 0;
-
-//         for (i=0;i<data.length; i++) {
-//             total=+data[i].progress;
-//         }
-//         return [total, data.length]
-//             } else {
-// let total = [0, 0];
-// for (let subData of Object.values(data)) {
-//     const subDataArr = getTotalProgressByRecursion(subData);
-//     total[0] +=subDataArr[0];
-//     total[1] =+subDataArr[1];
-// }
-// return total;
-//             }
-
-
-// }
-// const result  = getTotalProgressByRecursion(student);
-// console.log (result[0]/result[1]);
