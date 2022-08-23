7e43a50a8126c2ff291fd7aaf6c69e8c80f00372
Dmitriy <o01i@yandex.ru>
2022-08-23 11:12:10 +0300

    exe 61/19

--- script.js
+++ script.js
@@ -1,54 +1,9 @@
 'use strict';
-function isPangram(string) {
-let strAlfavit = 'abcdefghijklmnopqrstuvwxyz',
-    k=0;
-    string = string.split(' ').join('').toLowerCase();
-for (const letters of string) {
-    for (const letters1 of strAlfavit) {
-    if (letters1===letters) {
-        k+=1;
-        if (k===26) {
-            return true;
-        }
-    }
-
-    }
-
-}
-return false;
+function deepCount(a){
+    let count = a.length;
+    for (let i=0; i<a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+    return count;
 }
 
+deepCount([1, 2, [3, 4, [5]]]);
 
-
-
-
-isPangram('The qus over the lazy dog');
-
-
-
-
-
-
-
-
-
-
-
-// if (String(i).length==1) {
-        
-// }
-// if (String(i).length==2) {
-//     summArr.push(Number(String(i)[0]));
-//     summArr.push(Number(String(i)[1]));
-// }
-// if (String(i).length==3) {
-//     summArr.push(Number(String(i)[0]));
-//     summArr.push(Number(String(i)[1]));
-//     summArr.push(Number(String(i)[2]));
-// }
-// if (String(i).length==4) {
-//     summArr.push(Number(String(i)[0]));
-//     summArr.push(Number(String(i)[1]));
-//     summArr.push(Number(String(i)[2]));
-//     summArr.push(Number(String(i)[3]));
-// }
