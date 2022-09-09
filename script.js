9f9efb2903e1dd3b3ebeac5adf6856ecd73462a2
Dmitriy <o01i@yandex.ru>
2022-09-09 19:10:08 +0300

    exemple 88/20

--- script.js
+++ script.js
@@ -1,73 +1,71 @@
 'use strict';
-// const box = document.querySelector('.box'),
-// btn = document.querySelector('button');
-
-// const width = box.scrollWidth;
-// const height = box.scrollHeight; 
-// console.log(width, height);
-
-
-// btn.addEventListener('click', ()=> {
-// // box.style.height = box.scrollHeight  + 'px';
-// console.log(box.scrollTop);
-// });
-
-
-// console.log(box.getBoundingClientRect().top);
-
-// const style = window.getComputedStyle(box);
-// console.log(style.display);
-
-// console.log(document.documentElement.scrollTop)
-
-// class Rectangle {
-//     constructor(height, width) {
-//         this.height = height;
-//         this.width = width;
-//             }
-//             calcArea () {
-//                 return this.height*this.width;
-//             }
-// }
-// class ColirRactangleWithText extends Rectangle {
-//     constructor (height, width, text, bgColor) {
-//         super(height, width);
-//         this.text = text;
-//         this.bgColor = bgColor;
-
-//     }
-//     swowMyProps () {
-//         console.log(`Текст: ${this.text}, цвует: ${this.bgColor} `)
-//     }
-// }
-// const div = new ColirRactangleWithText (25,10, 'привет мир','красный');
-// div.swowMyProps();
-// console.log(div.calcArea());
 
 
 
-// 'use strict';
- 
-// function getSum(a, b) {
-//     function sum() {
-//         console.log(this.a);
-//         return a + b;
-//     }
- 
-//     console.log(sum());
-// }
- 
-// getSum(4, 5);
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
 
-// const square = new Rectangle(10,10);
-// const long = new Rectangle(20,100);
-// console.log(square.calcArea());
-// console.log(long.calcArea());
+function showGoodFilms(arr) {
+    return arr.filter((item=> item.rating>=8));
+    
+}
 
-// console.log(square);
-// console.log(Rectangle);
-const persone = {
-    name: 'Alex',
-    tel: '+744444444'
-};
-console.log(JSON.parse(JSON.stringify(persone)));
+function showListOfFilms(arr) {
+    return arr.map((item => item.name))
+    .join(', '); 
+    
+}
+
+function setFilmsIds(arr) {
+    arr.map((item,index) => {
+    item.id = index; 
+    } );
+    return arr;
+}
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) { 
+    return arr.reduce((acc, item) =>
+   ( acc && item.hasOwnProperty('id')));
+         
+   }
+   
+    
+    
+
+
+setFilmsIds(films);
+//console.log (showGoodFilms(films));
+//console.log (showListOfFilms(films));
+// console.log (tranformedArray);
+console.log(checkFilms(tranformedArray));
+
+
+
+
+
+
+
+
+
+
+
+
+
