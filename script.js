808d9966d199aceb199666f77a805e0c202407a6
Dmitriy <o01i@yandex.ru>
2022-09-11 13:07:09 +0300

    exemler 88/21

--- script.js
+++ script.js
@@ -1,61 +1,100 @@
 'use strict';
 
-
-
-const films = [
-    {
-        name: 'Titanic',
-        rating: 9
-    },
-    {
-        name: 'Die hard 5',
-        rating: 5
-    },
-    {
-        name: 'Matrix',
-        rating: 8
-    },
-    {
-        name: 'Some bad film',
-        rating: 4
-    }
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
 ];
 
-function showGoodFilms(arr) {
-    return arr.filter((item=> item.rating>=8));
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount > 0)
+    .reduce((w,y)=>w+y.amount,0)
+    };
+
+const getTotalIncomeAmount = (data) => {
+    data.some(item => item.amount<0) ? 
+      data.reduce((w,y)=>w+y.amount,0):
+      getPositiveIncomeAmount(data);
+    };
+
+// console.log(getPositiveIncomeAmount(funds));
+console.log(getTotalIncomeAmount(funds));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const films = [
+//     {
+//         name: 'Titanic',
+//         rating: 9
+//     },
+//     {
+//         name: 'Die hard 5',
+//         rating: 5
+//     },
+//     {
+//         name: 'Matrix',
+//         rating: 8
+//     },
+//     {
+//         name: 'Some bad film',
+//         rating: 4
+//     }
+// ];
+
+// function showGoodFilms(arr) {
+//     return arr.filter((item=> item.rating>=8));
     
-}
+// }
+// showGoodFilms(films);
 
-function showListOfFilms(arr) {
-    return arr.map((item => item.name))
-    .join(', '); 
+// function showListOfFilms(arr) {
+//     return arr.map((item => item.name))
+//     .join(', '); 
     
-}
+// }
 
-function setFilmsIds(arr) {
-    arr.map((item,index) => {
-    item.id = index; 
-    } );
-    return arr;
-}
+// function setFilmsIds(arr) {
+//     arr.map((item,index) => {
+//     item.id = index; 
+//     } );
+//     return arr;
+// }
 
-const tranformedArray = setFilmsIds(films);
+// const tranformedArray = setFilmsIds(films);
 
-function checkFilms(arr) { 
-    return arr.reduce((acc, item) =>
-   ( acc && item.hasOwnProperty('id')));
+// function checkFilms(arr) { 
+//     return arr.reduce((acc, item) =>
+//    ( acc && item.hasOwnProperty('id')));
          
-   }
+//    }
    
     
     
 
 
-setFilmsIds(films);
-//console.log (showGoodFilms(films));
-//console.log (showListOfFilms(films));
-// console.log (tranformedArray);
-console.log(checkFilms(tranformedArray));
+// setFilmsIds(films);
+// console.log (showGoodFilms(films));
+// console.log (showListOfFilms(films));
+// // console.log (tranformedArray);
+// console.log(checkFilms(tranformedArray));
 
 
 
